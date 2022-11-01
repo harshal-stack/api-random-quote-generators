380bf0d895f24b0c8b2cece7c970721aa24dc76f
HarshalDagde <99630886+harshal-stack@users.noreply.github.com>
2022-11-01 16:05:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-async function getQuote() {
-  ' -> this line;
-  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-try {
-
-     const response = await fetch(proxyUrl + apiUrl) -> this line;
-     const data = await response.json();
-     console.log({data});
-  } catch (error) {
-     console.log(error);
-      }
-  }
-// On load
-getQuote();
-
 
 async function quotesapi() {
     Const cors = 'https://cors-anywhere.herokuapp.com'
