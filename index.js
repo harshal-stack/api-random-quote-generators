e20334b7676d2928a6e1d78b336e672df07ce167
HarshalDagde <99630886+harshal-stack@users.noreply.github.com>
2022-11-01 16:04:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+async function getQuote() {
+  ' -> this line;
+  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+try {
+
+     const response = await fetch(proxyUrl + apiUrl) -> this line;
+     const data = await response.json();
+     console.log({data});
+  } catch (error) {
+     console.log(error);
+      }
+  }
+// On load
+getQuote();
+
 
 async function quotesapi() {
-    let quote = document.querySelector(".quote");
-    let anime = document.querySelector(".anime");
-    let author = document.querySelector(".author");
-    
-    let getdata = await fetch("https://animechan.vercel.app/api/random");
+    Const cors = 'https://cors-anywhere.herokuapp.com'
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/
+    let getdata = await fetch(`"https://animechan.vercel.app/api/random"${cors}`);
     let finaledata = await getdata.json();
     console.log(finaledata.anime)
     document.querySelector(".Quotes").innerHTML = finaledata.quote
